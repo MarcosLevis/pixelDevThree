889696017e923c11d741e459499d4036891b9d32
rck <marcoslevis.rck@gmail.com>
2024-03-03 17:53:15 -0300

    empezando el libro

--- src/main.js
+++ src/main.js
@@ -21,7 +21,7 @@ const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x99AD2D } );
 const cube = new THREE.Mesh( geometry, material);
 
-scene.add( cube, light);
+scene.add( cube);
 
 camera.position.z = 5;
 
